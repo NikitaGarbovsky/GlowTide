d4caf1de102f86dc8d6cd97c1ebaf02046d5da1c
Nikita_Laptop\Nikita_Work_Laptop <nikitawingategarbovsky@gmail.com>
2024-10-17 23:24:01 +1300

    Integrated Connors throwing & calling scripts into player prefab, Integrated new player-following camera into a prefab & in scenes, Began on intergration of door and seaslug interaction.

--- Assets/Scripts/DoorInteractiveObject.cs
+++ Assets/Scripts/DoorInteractiveObject.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
 public class DoorInteractiveObject : InteractiveObject
 {
-    [SerializeField] const int m_iObjectConditionAmount = 0;
+    [SerializeField] int m_iObjectConditionAmount = 0;
+    [SerializeField] GameObject SlugSpot1;
+    [SerializeField] GameObject SlugSpot2;
+    [SerializeField] GameObject SlugSpot3;
+    [SerializeField] GameObject SlugSpot4;
+    [SerializeField] GameObject SlugSpot5;
 
+    [SerializeField] GameObject Grid;
     private void Start()
     {
         m_iCondition = m_iObjectConditionAmount;
@@ -16,7 +23,17 @@ public class DoorInteractiveObject : InteractiveObject
     {
         if (GetSeaSlugListCount() >= m_iObjectConditionAmount)
         {
-            // Execute door specific code (remove it                                                                       
+            // Grabs the bounds of the door,
+            Bounds doorBounds = GetComponent<PolygonCollider2D>().bounds;
+            // Destroys it,
+            Destroy(gameObject);  
+            // Then updates the pathfinding graph (removes the collision) 
+            Grid.GetComponent<AstarPath>().UpdateGraphs(doorBounds);
         }
+    }
+
+    private void Update()
+    {
+        CheckIfActionConditionMet();
     }
 }
